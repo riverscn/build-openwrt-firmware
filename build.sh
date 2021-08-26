944dd2ca62f4ebf5b3b1b37c33ceed8dd5301b12
Shun Li <riverscn@gmail.com>
2021-08-27 07:45:27 +0800

    fix pack firmware

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@
 PREPARE_SCRIPT="prepare_env.sh"
 FETCH_OPENWRT_SCRIPT="fetch_openwrt.sh"
 COMPILE_OPENWRT_SCRIPT="compile_openwrt.sh"
-OPENWRT_VER="Immortal21.02"
 PACKAGED_OUTPUTPATH="/opt/openwrt_packit/tmp"
 
 (. ${PREPARE_SCRIPT})
--- pack_firmware.sh
+++ pack_firmware.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-cd openwrt
-rename -f 's/immortalwrt/openwrt/' bin/targets/*/*/*.img.gz
-rename -f "s/sysupgrade/${OPENWRT_VER}/" bin/targets/*/*/*.img.gz
-rm bin/targets/*/*/*factory.img.gz
-cd..
-
 OPENWRT_ARMVIRT="$(pwd)/openwrt/bin/targets/armvirt/64/*.tar.gz"
 PACKAGE_SOC="all"
 KERNEL_VERSION_NAME="5.4.142"
 OPENWRT_VER="Immortal21.02"
 WHOAMI="river"
 
+cd openwrt
+rename -f 's/immortalwrt/openwrt/' bin/targets/*/*/*.img.gz
+rename -f "s/sysupgrade/${OPENWRT_VER}/" bin/targets/*/*/*.img.gz
+rm bin/targets/*/*/*factory.img.gz
+cd..
+
 git clone https://github.com/unifreq/openwrt_packit.git || cd openwrt_packit && git stash && git pull && cd ..
-
 cd openwrt_packit
-
 source openwrt_flippy.sh
