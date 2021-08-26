e360e7a69ef974a38747fffa6226af59e36fcf53
Shun Li <riverscn@gmail.com>
2021-08-26 12:51:22 +0800

    better config method

---
+++ configs/arm64-immortal21.02-gateway-flowoffload-sfe.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cd immortalwrt
+
+packages=" \
+brcmfmac-firmware-43430-sdio brcmfmac-firmware-43455-sdio kmod-brcmfmac wpad \
+kmod-fs-ext4 kmod-fs-vfat kmod-fs-exfat dosfstools e2fsprogs ntfs-3g \
+kmod-usb2 kmod-usb3 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas \
+kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 \
+blkid lsblk parted fdisk cfdisk losetup resize2fs tune2fs pv unzip \
+lscpu htop iperf3 curl lm-sensors python3 autocore-arm
+"
+sed -i '/FEATURES+=/ { s/cpiogz //; s/ext4 //; s/ramdisk //; s/squashfs //; }' \
+    target/linux/armvirt/Makefile
+for x in $packages; do
+    sed -i "/DEFAULT_PACKAGES/ s/$/ $x/" target/linux/armvirt/Makefile
+done
+
+cat >.config <<-EOF
+## target
+CONFIG_TARGET_armvirt=y
+CONFIG_TARGET_armvirt_64=y
+CONFIG_TARGET_armvirt_64_Default=y
+
+## luci app
+CONFIG_PACKAGE_luci-app-argon-config=y
+CONFIG_PACKAGE_luci-app-passwall=y
+CONFIG_PACKAGE_luci-app-udpxy=y
+CONFIG_PACKAGE_ipv6helper=y
+CONFIG_PACKAGE_luci-app-upnp=y
+
+## luci theme
+CONFIG_PACKAGE_luci-theme-argon=y
+
+## remove
+# CONFIG_PACKAGE_luci-app-webadmin is not set
+# CONFIG_PACKAGE_luci-app-vlmcsd is not set
+# CONFIG_PACKAGE_luci-app-ssr-plus is not set
+
+## others
+CONFIG_BRCMFMAC_SDIO=y
+CONFIG_LUCI_LANG_en=y
+EOF
+
+make defconfig
+cat .config
