eb1e2d0fa153ba2b09c03c6dd8c149273ff8fb01
Shun Li <riverscn@gmail.com>
2021-08-27 08:09:19 +0800

    cd openwrt

--- fetch_openwrt.sh
+++ fetch_openwrt.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-git clone -b openwrt-21.02 --single-branch https://github.com/immortalwrt/immortalwrt openwrt || cd openwrt && git stash && git pull && cd ..
-
-cd openwrt
+git clone -b openwrt-21.02 --single-branch https://github.com/immortalwrt/immortalwrt openwrt && cd openwrt || cd openwrt && git stash && git pull
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
