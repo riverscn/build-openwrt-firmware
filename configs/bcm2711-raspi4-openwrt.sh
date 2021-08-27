78c447253378d7b551b2e3098f664bcde52a318f
Shun Li <riverscn@gmail.com>
2021-08-28 00:11:04 +0800

    add usb support etc.

--- configs/bcm2711-raspi4-openwrt.sh
+++ configs/bcm2711-raspi4-openwrt.sh
@@ -15,4 +15,21 @@ CONFIG_PACKAGE_kmod-usb-net-asix-ax88179=y
 CONFIG_PACKAGE_kmod-usb-net-rtl8150=y
 CONFIG_PACKAGE_kmod-usb-net-rtl8152=y
 
+## USB Storage
+CONFIG_PACKAGE_kmod-usb-storage=y
+CONFIG_PACKAGE_kmod-usb-storage-extras=y
+
+## USB utils
+CONFIG_PACKAGE_usbutils=y
+
+## File System
+CONFIG_PACKAGE_fdisk=y
+CONFIG_PACKAGE_kmod-fs-exfat=y
+CONFIG_PACKAGE_kmod-fs-ext4=y
+CONFIG_PACKAGE_kmod-fs-vfat=y
+CONFIG_PACKAGE_kmod-fs-ntfs=y
+
+## Language
+CONFIG_PACKAGE_kmod-nls-cp936=y
+
 EOF
--- configs/x86-openwrt.sh
+++ configs/x86-openwrt.sh
@@ -16,4 +16,21 @@ CONFIG_PACKAGE_luci-app-omcproxy=y
 CONFIG_PACKAGE_luci-app-sqm=y
 CONFIG_PACKAGE_acme-dnsapi=y
 
+## USB Storage
+CONFIG_PACKAGE_kmod-usb-storage=y
+CONFIG_PACKAGE_kmod-usb-storage-extras=y
+
+## USB utils
+CONFIG_PACKAGE_usbutils=y
+
+## File System
+CONFIG_PACKAGE_fdisk=y
+CONFIG_PACKAGE_kmod-fs-exfat=y
+CONFIG_PACKAGE_kmod-fs-ext4=y
+CONFIG_PACKAGE_kmod-fs-vfat=y
+CONFIG_PACKAGE_kmod-fs-ntfs=y
+
+## Language
+CONFIG_PACKAGE_kmod-nls-cp936=y
+
 EOF
