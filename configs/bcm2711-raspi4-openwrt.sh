d0babbe0293390d2075e463bd97794ad3c855218
Shun Li <riverscn@gmail.com>
2021-08-27 23:14:46 +0800

    RPI support usb ethernet

--- configs/bcm2711-raspi4-openwrt.sh
+++ configs/bcm2711-raspi4-openwrt.sh
@@ -8,4 +8,11 @@ CONFIG_TARGET_bcm27xx=y
 CONFIG_TARGET_bcm27xx_bcm2711=y
 CONFIG_TARGET_bcm27xx_bcm2711_DEVICE_rpi-4=y
 
+## USB Ethernet
+CONFIG_PACKAGE_kmod-usb-net-aqc111=y
+CONFIG_PACKAGE_kmod-usb-net-asix=y
+CONFIG_PACKAGE_kmod-usb-net-asix-ax88179=y
+CONFIG_PACKAGE_kmod-usb-net-rtl8150=y
+CONFIG_PACKAGE_kmod-usb-net-rtl8152=y
+
 EOF
