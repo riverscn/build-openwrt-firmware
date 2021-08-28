178a18557d337053caa4e028977aaf004eaa5765
Shun Li <riverscn@gmail.com>
2021-08-28 15:13:23 +0800

    Update compile_openwrt.sh

--- compile_openwrt.sh
+++ compile_openwrt.sh
@@ -3,17 +3,31 @@
 rm -rf bin/ 2> /dev/null
 mkdir bin
 
-for FILE in ./configs/*-openwrt.sh
-do
-    (
-        echo "Free space:"
-        df -h
-        echo "Compiling: ${FILE}"
-        bash update_config.sh ${FILE}
-        cd openwrt
-        make download -j8
-        make -j$(nproc) || make -j1 V=s
-        mv bin/targets/*/*/*.*.gz ../bin/
-        if [ "$1" = "actions" ]; then make clean; fi
-    )
-done
+if [ -z "$1" or "$1" != "actions" ]
+  then
+    for FILE in ./configs/*-openwrt.sh
+        do
+            (
+                echo "Free space:"
+                df -h
+                echo "Compiling: ${FILE}"
+                bash update_config.sh ${FILE}
+                cd openwrt
+                make download -j8
+                make -j$(nproc) || make -j1 V=s
+                mv bin/targets/*/*/*.*.gz ../bin/
+                if [ "$1" = "actions" ]; then make clean; fi
+            )
+        done
+  else
+    echo "Free space:"
+    df -h
+    echo "Compiling: $1"
+    bash update_config.sh $1
+    cd openwrt
+    make download -j8
+    make -j$(nproc) || make -j1 V=s
+    mv bin/targets/*/*/*.*.gz ../bin/
+fi
+
+
