0e372587c484c9a1f51dade8156fd2d0dcb26d17
Shun Li <riverscn@gmail.com>
2021-08-28 16:28:17 +0800

    build multi

--- compile_openwrt.sh
+++ compile_openwrt.sh
@@ -16,7 +16,7 @@ if [ -z "$1" or "$1" != "all" ]
                 make download -j8
                 make -j$(nproc) || make -j1 V=s
                 mv bin/targets/*/*/*.*.gz ../bin/
-                if [ "$1" = "actions" ]; then make clean; fi
+                if [ "$1" = "all" ]; then make clean; fi
             )
         done
   else
@@ -30,4 +30,14 @@ if [ -z "$1" or "$1" != "all" ]
     mv bin/targets/*/*/*.*.gz ../bin/
 fi
 
+KERNEL_VERSION_ORIG="$(awk '/kernel/ {print $3}' $(pwd)/openwrt/bin/targets/*/*/*.manifest | awk -F '-' '{print $1}' | awk 'NR==1')"
 
+rename -f 's/immortalwrt/openwrt/' bin/*.*.gz
+rename -f "s/sysupgrade/${OPENWRT_VER}_k${KERNEL_VERSION_ORIG}/" bin/*.img.gz
+rename -f "s/combined/combined-${OPENWRT_VER}_k${KERNEL_VERSION_ORIG}/" bin/*.img.gz
+
+rm bin/*rpi*factory.img.gz
+rm bin/*rootfs.img.gz
+rm bin/*ext4*.img.gz
+
+ls bin/
--- pack_firmware.sh
+++ pack_firmware.sh
@@ -6,16 +6,4 @@ KERNEL_VERSION_NAME="5.4.142"
 OPENWRT_VER="immortal21.02"
 WHOAMI="river"
 
-KERNEL_VERSION_ORIG="$(awk '/kernel/ {print $3}' $(pwd)/openwrt/bin/targets/*/*/*.manifest | awk -F '-' '{print $1}' | awk 'NR==1')"
-
-rename -f 's/immortalwrt/openwrt/' bin/*.*.gz
-rename -f "s/sysupgrade/${OPENWRT_VER}_k${KERNEL_VERSION_ORIG}/" bin/*.img.gz
-rename -f "s/combined/combined-${OPENWRT_VER}_k${KERNEL_VERSION_ORIG}/" bin/*.img.gz
-
-rm bin/*rpi*factory.img.gz
-rm bin/*rootfs.img.gz
-rm bin/*ext4*.img.gz
-
-ls bin/
-
 source openwrt_flippy.sh
