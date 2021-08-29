d77ce60c0707366a50dc94dfba9b9d836bba9cb2
Shun Li <riverscn@gmail.com>
2021-08-29 19:01:30 +0800

    Update arm64-openwrt.sh

--- configs/arm64-openwrt.sh
+++ configs/arm64-openwrt.sh
@@ -8,7 +8,7 @@ kmod-fs-ext4 kmod-fs-vfat kmod-fs-exfat dosfstools e2fsprogs ntfs-3g \
 kmod-usb2 kmod-usb3 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas \
 kmod-usb-net kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 \
 blkid lsblk parted fdisk cfdisk losetup resize2fs tune2fs pv unzip \
-lscpu htop iperf3 curl lm-sensors python3 autocore-arm
+lscpu htop iperf3 curl lm-sensors python3 autocore-arm libopenssl-afalg
 "
 sed -i '/FEATURES+=/ { s/cpiogz //; s/ext4 //; s/ramdisk //; s/squashfs //; }' \
     target/linux/armvirt/Makefile
