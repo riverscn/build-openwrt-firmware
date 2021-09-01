760bb448cf2c8e1bfd473e96908c5cbbc224a2bc
Shun Li <riverscn@gmail.com>
2021-09-01 15:39:40 +0800

    Update pack_firmware.sh

--- pack_firmware.sh
+++ pack_firmware.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPENWRT_ARMVIRT="$(pwd)/bin/openwrt-armvirt-64-default-rootfs.tar.gz"
+OPENWRT_ARMVIRT="$(pwd)/bin/tmp/openwrt-armvirt-64-default-rootfs.tar.gz"
 PACKAGE_SOC="s905d"
 KERNEL_VERSION_NAME="5.4.142"
 OPENWRT_VER="immortal21.02"
