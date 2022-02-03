3b99eef85ede2ffa7aa06aa4d35ca7395396af8f
Shun Li <riverscn@gmail.com>
2022-02-02 20:08:07 -0800

    no iptv no argon theme

--- configs/x86-openwrt.sh
+++ configs/x86-openwrt.sh
@@ -10,8 +10,6 @@ CONFIG_TARGET_x86_64_DEVICE_generic=y
 
 ## app
 CONFIG_PACKAGE_luci-app-acme=y
-CONFIG_PACKAGE_luci-app-iptvhelper=y
-CONFIG_PACKAGE_luci-app-mwan3=y
 CONFIG_PACKAGE_luci-app-omcproxy=y
 CONFIG_PACKAGE_luci-app-sqm=y
 CONFIG_PACKAGE_acme-dnsapi=y
