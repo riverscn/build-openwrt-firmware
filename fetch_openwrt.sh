27e284aa455611d497749eb20ef2888fec4bc75b
Shun Li <riverscn@gmail.com>
2022-05-16 16:03:04 +0800

    change to openwrt-21.02

--- fetch_openwrt.sh
+++ fetch_openwrt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(git clone -b master --single-branch https://github.com/immortalwrt/immortalwrt openwrt) || (cd openwrt && git stash && git pull)
+(git clone -b openwrt-21.02 --single-branch https://github.com/immortalwrt/immortalwrt openwrt) || (cd openwrt && git stash && git pull)
 
 cd openwrt
 
