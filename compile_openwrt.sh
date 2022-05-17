441428e84e584a2a9959fdb8739cd7f0c57bbe06
Shun Li <riverscn@gmail.com>
2022-05-17 15:23:06 +0800

    more default settings

--- compile_openwrt.sh
+++ compile_openwrt.sh
@@ -38,9 +38,6 @@ for FILE in $@
       # if [ "$(df --output=avail -h $(pwd) | sed '1d;s/[^0-9]//g')" -lt "20" ]; then make dirclean; fi
       cd ..
       rename -f "s/immortalwrt/openwrt/" bin/tmp/*.*.gz
-      rm bin/tmp/*rpi*factory.img.gz 2> /dev/null
-      rm bin/tmp/*rootfs.img.gz 2> /dev/null
-      rm bin/tmp/*ext4*.img.gz 2> /dev/null
 
       if [ "$USE_FLIPPY_KERNEL" = "1" ]; then
         # Pack Other SBC Boxes' firmware
@@ -52,6 +49,6 @@ for FILE in $@
         KERNEL_VERSION_ORIG="$(awk '/kernel/ {print $3}' $(pwd)/openwrt/bin/targets/*/*/*.manifest | awk -F '-' '{print $1}' | awk 'NR==1')"
         rename -f "s/.img.gz/-${OPENWRT_VER}_k${KERNEL_VERSION_ORIG}${CONFIG_NAME}.img.gz/" bin/tmp/*.img.gz
       fi
-      mv bin/tmp/*.img.gz bin/
+      mv bin/tmp/*squashfs-combined*.img.gz bin/
     done
 rm bin/tmp/ -r
