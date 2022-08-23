7c0363a0ad3e24f398d8c6b22d2c2a4feda8c7f1
Shun Li <riverscn@gmail.com>
2022-08-23 14:18:58 +0800

    Update pack_firmware.sh

--- pack_firmware.sh
+++ pack_firmware.sh
@@ -3,7 +3,7 @@
 OPENWRT_ARMVIRT="$(pwd)/bin/tmp/openwrt-armvirt-64-default-rootfs.tar.gz"
 PACKAGE_SOC="s905d"
 KERNEL_VERSION_NAME="5.4.142"
-OPENWRT_VER="immortal22.02"
+OPENWRT_VER="immortal22.03"
 WHOAMI="river"
 
 source openwrt_flippy.sh
