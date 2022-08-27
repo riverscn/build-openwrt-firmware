8e71435649be638143bb8b7a69f6016d9053e734
Shun Li <riverscn@gmail.com>
2022-08-27 13:36:38 +0800

    Update fetch_openwrt.sh

--- fetch_openwrt.sh
+++ fetch_openwrt.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-(git clone https://github.com/immortalwrt/immortalwrt -b master openwrt) || (cd openwrt && git stash && git pull)
+(git clone https://github.com/immortalwrt/immortalwrt openwrt) || (cd openwrt && git stash && git pull)
 
 cd openwrt
+
+(git clone https://github.com/xiaorouji/openwrt-passwall2 ./packages/openwrt-passwall2) || (cd ./package/lean && git stash && git pull && cd ../.. )
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
